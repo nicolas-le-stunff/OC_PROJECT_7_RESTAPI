4027b320ee3bc4d6ee8cf3343dd4bcc7193b7db0
Nicolas.LeStunff <nicolas.le-stunff@hotmail.fr>
2022-04-29 16:58:31 +0200

    add security config

---
+++ Poseiden-skeleton/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,81 @@
+package config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    
+    @Autowired
+    DataSource dataSource;
+    
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+	public boolean isAuthenticated() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || AnonymousAuthenticationToken.class.
+				isAssignableFrom(authentication.getClass())) {
+			return false;
+		}
+		return authentication.isAuthenticated();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(encoder());
+		return authProvider;
+	}
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+	http.authorizeRequests()
+		.antMatchers("/css/**").permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.usernameParameter("username")
+		.passwordParameter("password")
+		.failureUrl("/login?error")
+		.defaultSuccessUrl("/bidList/list")
+		.and()
+		.logout()
+		.logoutSuccessUrl("/login?logout").permitAll()
+		.and()
+		.oauth2Login()
+		.loginPage("/login").permitAll()
+		.failureUrl("/login?error")
+		.defaultSuccessUrl("/bidList/list")
+		.and()
+		.logout()
+		.logoutSuccessUrl("/login?logout").permitAll();
+    }
+}
