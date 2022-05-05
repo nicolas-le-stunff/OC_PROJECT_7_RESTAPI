90ccc9913d5dcc81a8d4f63318739cae42ebad8e
Nicolas.LeStunff <nicolas.le-stunff@hotmail.fr>
2022-05-06 01:56:20 +0200

    Update fix RuleName

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -1,6 +1,11 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.service.RuleNameService;
+
+import javassist.NotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,46 +14,68 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 @Controller
 public class RuleNameController {
-    // TODO: Inject RuleName service
+   
+	@Autowired
+	private RuleNameService ruleNameService;
 
     @RequestMapping("/ruleName/list")
-    public String home(Model model)
-    {
-        // TODO: find all RuleName, add to model
+    public String home(Model model){
+        List<RuleName> ruleNameList = ruleNameService.getAllRuleName();
+        model.addAttribute("ruleNameList",ruleNameList);
         return "ruleName/list";
     }
 
     @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleName bid) {
+    public String addRuleForm(Model model) {
+    	RuleName ruleName = new RuleName();
+    	model.addAttribute("ruleName",ruleName);
         return "ruleName/add";
     }
 
     @PostMapping("/ruleName/validate")
     public String validate(@Valid RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
-        return "ruleName/add";
+        if(result.hasErrors()) {
+        	model.addAttribute("ruleName",ruleName);
+        	return "ruleName/add";
+        }
+        ruleNameService.createRuleName(ruleName);
+        return "redirect:/ruleName/list";
     }
 
     @GetMapping("/ruleName/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
-        return "ruleName/update";
+        try {
+        	RuleName ruleName = ruleNameService.getRuleNameById(id);
+        	model.addAttribute("ruleName",ruleName);
+        	  return "ruleName/update";
+        }catch(NotFoundException e) {
+        	return "notFound";
+        }
     }
 
     @PostMapping("/ruleName/update/{id}")
-    public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
-        return "redirect:/ruleName/list";
+    public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName, BindingResult result, Model model) {
+        try {
+        	if(result.hasErrors()) {
+        		model.addAttribute("ruleName",ruleName);
+        		return "ruleName/update";
+        	}
+        	ruleNameService.updateRuleName(id, ruleName);
+        	 return "redirect:/ruleName/list";
+        }catch (NotFoundException e) {
+        	return "notFound";
+        }
     }
 
     @GetMapping("/ruleName/delete/{id}")
     public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
+        ruleNameService.deleteRuleName(id);
         return "redirect:/ruleName/list";
     }
 }
