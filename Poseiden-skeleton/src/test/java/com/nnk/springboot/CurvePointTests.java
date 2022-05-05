a1fe2d9f36e7cb0b31c0efb4dfbd42e6976baae0
Nicolas.LeStunff <nicolas.le-stunff@hotmail.fr>
2022-05-06 01:56:50 +0200

    add some tests

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/CurvePointTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/CurvePointTests.java
@@ -2,12 +2,20 @@ package com.nnk.springboot;
 
 import com.nnk.springboot.domain.CurvePoint;
 import com.nnk.springboot.repositories.CurvePointRepository;
+import com.nnk.springboot.service.CurvePointService;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,17 +24,51 @@ import java.util.Optional;
 @SpringBootTest
 public class CurvePointTests {
 
-/*	@Autowired
+	@Mock
 	private CurvePointRepository curvePointRepository;
+	
+	@InjectMocks
+	private CurvePointService curvePointService;
 
 	@Test
+	public void createCurvePointTest() {
+		Mockito.verify(curvePointRepository,Mockito.times(0)).save(any());
+		
+		CurvePoint curve = new CurvePoint();
+		curve.setId(1);
+		curve.setCurveId(2);
+		curve.setTerm(3);
+		curve.setValue(4);
+		
+		curvePointService.createCurvePoint(curve);
+		
+		Mockito.verify(curvePointRepository,Mockito.times(1)).save(any());
+		
+	}
+	
+	@Test
+	public void updateCurvePointTest() {		
+		CurvePoint curve = new CurvePoint();
+		curve.setId(1);
+		curve.setCurveId(2);
+		curve.setTerm(3);
+		curve.setValue(4);
+		
+		CurvePoint curveUpdate = new CurvePoint();
+		curve.setId(1);
+		curve.setCurveId(2);
+		curve.setTerm(5);
+		curve.setValue(4);
+		
+	
+	
+		
+	}
+	/*@Test
 	public void curvePointTest() {
 		CurvePoint curvePoint = new CurvePoint(10, 10d, 30d);
 
-		// Save
-		curvePoint = curvePointRepository.save(curvePoint);
-		Assert.assertNotNull(curvePoint.getId());
-		Assert.assertTrue(curvePoint.getCurveId() == 10);
+
 
 		// Update
 		curvePoint.setCurveId(20);
