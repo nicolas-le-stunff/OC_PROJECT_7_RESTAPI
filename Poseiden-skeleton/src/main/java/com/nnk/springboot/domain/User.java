0d3581eecfac217f189c5abeaf9e64aa426354d4
unknown <nicolas.le-stunff@hotmail.fr>
2022-06-17 22:11:41 +0200

    update password

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
@@ -1,6 +1,8 @@
 package com.nnk.springboot.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,11 +20,14 @@ public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     Integer id;
-    
+
+	@NotBlank(message = "UserName is mandatory")
     String username;
     
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$", message = "The password must contain at least one capital letter, one number, one symbol and be at least 8 characters long")
     String password;
-    
+
+	@NotBlank(message = "FullName is mandatory")
     String fullname;
     
     String role;
